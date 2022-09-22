83fe9bdc6a77b85bf5defba6b9ac0e21d171ebd1
Code Ninja <fbook3603@gmail.com>
2022-09-23 03:27:02 +0600

    change some js file

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-document.getElementById('buy-now-btn').addEventListener('click' , function(){
-    const penguinsId = document.getElementById('penguins');
-    penguinsId.innerText = 'I Am Penguins';
-})
+document.getElementById("buy-now-btn").addEventListener("click", function () {
+  const penguinsId = document.getElementById("penguins");
+  penguinsId.innerText = "I Am Penguins";
+});
+
+document
+  .getElementById("penguins-sections")
+  .addEventListener("mouseleave", function () {
+    const offterOFF = document.getElementById("offer");
+    offterOFF.innerHTML = `
+    <h1 class="text-5xl bg-red-500 px-72 py-10 rounded-lg text-white font-bold">25% OFF</h1>
+    `;
+  });
