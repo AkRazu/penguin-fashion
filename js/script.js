51788e509277a6030f4d0bcb16a855dfab9db7cd
Code Ninja <fbook3603@gmail.com>
2022-09-23 03:07:54 +0600

    js file add

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('buy-now-btn').addEventListener('click' , function(){
+    const penguinsId = document.getElementById('penguins');
+    penguinsId.innerText = 'I Am Penguins';
+})
