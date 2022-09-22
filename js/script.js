75a3dba2385ddcaed224e4cf4557f1f26309c464
Code Ninja <fbook3603@gmail.com>
2022-09-23 03:41:51 +0600

    çhange add

--- js/script.js
+++ js/script.js
@@ -8,6 +8,6 @@ document
   .addEventListener("mouseleave", function () {
     const offterOFF = document.getElementById("offer");
     offterOFF.innerHTML = `
-    <h1 class="text-5xl bg-red-500 px-72 py-10 rounded-lg text-white font-bold">25% OFF</h1>
+    <h1 class="animate-pulse text-5xl bg-red-500 px-72 py-10 rounded-lg text-white font-bold">UP TO 75% SALE</h1>
     `;
   });
